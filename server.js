e17a6994cec9e313dd385fbf2fbbfb03027bf0b5
NIXON <nikimghanga@gmail.com>
2024-06-19 13:12:07 +0300

    database connected to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connectDB from'./config/boma_db.js'
  
 const app = express();
 const PORT = process.env.MONGO_URI || 3000;
@@ -6,6 +7,7 @@ const PORT = process.env.MONGO_URI || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
  
+connectDB();
 app.get("/", (request, response) => {
   response.send({ message: "Hello from an Express API!" });
 });
