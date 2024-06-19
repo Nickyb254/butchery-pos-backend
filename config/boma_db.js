d37d6557ec8fa8702c88a8a10b0c152f262ff3f4
NIXON <nikimghanga@gmail.com>
2024-06-19 13:11:11 +0300

    renamed database

---
+++ config/boma_db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+ 
+export default function connectDB() {
+  //const url = process.env.MONGO_URI;
+  
+  const url = "mongodb://127.0.0.1/boma_db";
+  try {
+    mongoose.connect(url, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    console.log(`Database connected: ${url}`);
+  });
+ 
+  dbConnection.on("error", (err) => {
+    console.error(`connection error: ${err}`);
+  });
+  return;
+}
