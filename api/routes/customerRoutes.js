92bc188c0ff9afeaa38c41eea3151a3658cd4cac
NIXON <nikimghanga@gmail.com>
2024-06-21 18:01:15 +0300

    update route not working, Get by Id not working, Get all router - Error: Cannot set headers after they are sent to the client in the console

--- api/routes/customerRoutes.js
+++ api/routes/customerRoutes.js
@@ -1,20 +1,45 @@
 import express from "express";
 const router = express.Router();
 
+import mongoose from "mongoose";
+import CustomerModel  from '../models/customers.js';
+
+
+
+
 router.get('/', (request, response, next) => {
+  CustomerModel.find()
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      response.status(200).json(doc);
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(500).json({
+        error:error
+      });
+    });
   response.status(200).json({
     message: 'Get Request from a customer!'
   });
 });
 
 
+
 router.post('/', (request, response, next) => {
-  const customer = {
-    customer_id: request.body.customer_id,
+  //the request being posted
+  const customer = new CustomerModel({
     customer_name: request.body.customer_name,
     customer_phone: request.body.customer_phone,
-  };
-  
+  });
+  customer
+    .save()
+    .then(result =>{
+      console.log(result);
+    })
+    .catch(error => console.log(error));
+
   response.status(200).json({
     message: 'Post Reqest from a customer!',
     customer: customer
@@ -22,18 +47,66 @@ router.post('/', (request, response, next) => {
 });
 
 
-router.patch('/:customersId',(request, response, next)=>{
-  response.status(200).json({
-    message: 'Patch Request from a customer!'
-  })
+
+
+// router.patch('/:customersId',(request, response, next)=>{
+//   const customer = [request.params.body];
+//   const id = request.params.customersId;
+//   const updateOps = {};
+//    for (const ops of request.body){
+//     updateOps[ops.propName] = ops.value;
+//    }
+//    customer.update({ _id: id}, { $set: updateOps })
+//    .exec()
+//    .then(result => {
+//     console.log(result);
+//     response.status(200).json({
+//       message: 'Patch Request from a customer!'
+//     })
+//    })
+//    .catch(error => {
+//     console.log(error);
+//     response.status(500)
+//    });
+  
+// });
+
+router.patch('/:customersId', async (req, res) => {
+  try {
+    const updateData = req.body;
+    const result = await CustomerModel.updateOne({ customer_id: req.params.id }, updateData);
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server error', error });
+  }
 });
 
 
-router.delete('/:customersId',(reqest, response, next)=>{
-  response.status(200).json({
-    message: 'Delete Request from a customer'
-  })
+
+
+
+router.delete('/:customersId', async (req, res) => {
+  try {
+    const result = await CustomerModel.deleteOne({ customer_id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server error', error });
+  }
 });
+
+
 
 
 export default router;
