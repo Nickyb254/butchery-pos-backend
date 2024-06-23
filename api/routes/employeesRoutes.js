efd5864ff2343f33c1ff9059682c8603d50e4462
NIXON <nikimghanga@gmail.com>
2024-06-23 19:09:31 +0300

    added find employee with Id along with validation error handling

--- api/routes/employeesRoutes.js
+++ api/routes/employeesRoutes.js
@@ -21,44 +21,55 @@ router.get('/', (request, response, next) => {
 });
 
 
-router.post('/', (request, response, next) => {
+//try block was added to catch validation errors. 
+//note {.} is removed in catch
+//made async thus used await
+router.post('/', async (request, response, next) => {
+  try{
   const employee = new EmployeeModel ({
     employee_Id: mongoose.Types.ObjectId,
     employee_name: request.body.employee_name,
     designation: request.body.designation,
-    phone_number: request.body.phone_number
+    phone_number: request.body.phone_number,
+    email: request.body.email,
+    password: request.body.password
   });
 
-  employee
+  await employee
   .save()
   .then(result => {
     console.log(result);
-  })
-  .catch(error => console.log(error));
-
+    
   response.status(200).json({
-    message: 'Post Request from an employee!',
+    message: 'New Employee created!',
     employee: employee,
   });
+  })}
+  //.catch(error => console.log(error));
+  catch (error) {
+    
+    response.status(500).json(error);
+  }
+
 });
 
 //GET by ID
-// router.get('/:employeeId', async(request, response, next) => {
-//   try{
-//     const customerId = request.params._Id;
-//     const result = await CustomerModel.findOne({ customerId: _Id });
+router.get('/:employeeId', async(request, response, next) => {
+  try{
+    const _Id = request.params.employee_Id;
+    const result = await EmployeeModel.findOne({ employeeId: _Id });
 
-//     if (!result) {
-//       console.log(request.params.body);
-//       return response.status(404).json({ message: 'Customer not found' });
-//     }
-//     console.log(request.params.body);
-//     response.status(200).json(result);    
-//   } catch (error) {
-//     console.log(error);
-//     response.status(500).json({ message: 'Server error', error });
-//   }
-// });
+    if (!result) {
+      console.log(request.params.body);
+      return response.status(404).json({ message: 'Employee not found' });
+    }
+    console.log(request.params.body);
+    response.status(200).json(result);    
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: 'Server error', error });
+  }
+});
 
 
 router.patch('/:employeesId', (request, response, next)=>{
