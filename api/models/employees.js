3cbc112b0f6b2beb14a607e655a7b9403baae239
NIXON <nikimghanga@gmail.com>
2024-06-23 19:06:54 +0300

    added email & password to the employee schema with validation

--- api/models/employees.js
+++ api/models/employees.js
@@ -6,6 +6,36 @@ const EmployeeSchema = new mongoose.Schema({
   employee_name:  String,
   designation: String,
   phone_number:  String,
+
+  email: {
+    type: String,
+    required: [true, "Email is a required field"],
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate(value) {
+      if (!value) {
+        throw new Error("Please enter a valid E-mail!");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Password is a required field"],
+    validate(value) {
+      //this will always throw a validation error
+      // if (!value || (value, { min: 6, max: 1000 })) {
+      //   throw Error("Length of the password should be between 6-1000");
+      // }         
+
+      if (value.toLowerCase().includes("password")) {
+        throw Error(
+          'The password should not contain the keyword "password"!'
+        );
+      }
+    }
+  },
+  //timestamps: true,
 });
 
 const EmployeeModel = mongoose.model('Employees', EmployeeSchema);
