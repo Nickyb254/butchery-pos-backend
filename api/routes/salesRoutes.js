9e270270618bcd2e2b6b83a2906cbb3f693c381d
NIXON <nikimghanga@gmail.com>
2024-06-24 13:53:05 +0300

    GET & POST working well with error handling. DELETE not filtering for specific Id

--- api/routes/salesRoutes.js
+++ api/routes/salesRoutes.js
@@ -1,13 +1,39 @@
 import express from 'express';
 const router =  express.Router();
 import salesModel from '../models/sales.js';
+import mongoose from 'mongoose';
+
+//to filter for products in stock
+import Stock from '../models/stock.js';
 
 router.get('/', (request, response, next)=>{
   salesModel.find()
+    .select('product price mass_sold transaction_by')
     .exec()
     .then(result => {
-      console.log(result);
-      response.status(200).json(result);
+      console.log(result);      
+      if(result.length < 1){
+        return response.status(200).json({message: 'No sales records available!'});
+      }
+      else{
+        response.status(200).json({
+          count: result.length,
+          saleProducts: result.map(result =>{
+            return{
+              _id: result._id,
+              product: result.product,
+              price: result.price,
+              mass: result.mass_sold,
+              servedBy: result.transaction_by,
+              request: {
+                type: 'GET',
+                url: 'http://localhost:3000/sales'
+              },
+              saleDate: result.Date
+            }
+          })
+        });  
+      }         
     })
     .catch(error => {
       console.log(error);
@@ -18,24 +44,41 @@ router.get('/', (request, response, next)=>{
   });
 
 
-router.post('/', (request, response, next) => {
-  const sale = new salesModel ({
-    product_name: request.body.product_name,
-    price: request.body.price,
-    mass_sold: request.body.mass_sold,
-    supplier_name: request.body.supplier_name,
-  });
-  sale
-    .save()
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => console.log(error));
-
-  response.status(200).json({
-    message: 'Post request from Sales successful!',
-    sale: sale,
-  });
+router.post('/',  (request, response, next) => {
+  Stock.findById(request.body.productId)
+                .then(product =>{
+                  try{
+                    const sale = new salesModel ({
+                      product: request.body.product,
+                      price: request.body.price,
+                      mass_sold: request.body.mass_sold,
+                      transaction_by: request.body.transaction_by
+                    });
+                  
+                   sale
+                    .save()
+                    .then(result => {
+                      console.log(result);
+                      response.status(200).json({
+                        message: 'Post request from Sales successful!',
+                        sale: {
+                          product: result._id,
+                          price: result.price,
+                          mass_sold: result.mass_sold,
+                          request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/sales'
+                          },
+                        }      
+                    }); 
+                    })}
+                    catch(error){
+                      response.status(500).json({
+                        message: 'Product does not exist!',
+                        error: error
+                      });
+                    };
+                })    
 });
 
 
@@ -64,18 +107,14 @@ router.delete('/:salesId', async (request, response, next)=>{
     if(result.deletedCount === 1){
       return response.status(200).json({message: 'Sale item deleted!'})
     }
-    else if(!result.deletedCount){
-      return response.status(404).json({message: 'No sale item found!'});
+    else {
+      return response.status(404).json({message: 'No sale item found with matching ID!'});
     }
   }
   catch(error){
     console.log(error);
     response.status(500).json({message: 'Server error', error});
   }
-  //started testing out this way
-  // response.status(200).json({
-  //   message: 'Delete request from Sales successful!'
-  // });
 });
 
 export default router;
