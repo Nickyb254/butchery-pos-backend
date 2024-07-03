9975a2220839db1bf274d4cd16636f6e8e585469
NIXON <nikimghanga@gmail.com>
2024-07-03 09:31:38 +0300

    installed react-router-dom; fixed Warning: React.jsx: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.

--- Z frontEnd/boma/src/App.jsx
+++ Z frontEnd/boma/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import Navbar from './components/Navbar.jsx';
 import Sidebar from './components/sidebar.jsx';
 import { 
@@ -12,24 +12,52 @@ import {
   UserRegistration
 } from './section';
 
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+const fetchData = async () => {
+  const {data} = await axios.post('http://localhost:${PORT}');
+  console.log(data);
+}
 
 function App() {
-  
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
-    <main className='relative'>
+    <main>
       <Navbar />
       <Sidebar />
       <Hero />
-      <CustomerRegistration />
-      <EmployeeRegistration />
-      <SaleRegistration />
-      <StockRegistration />
-      <UserRegistration />
-      <Footer />
+    <BrowserRouter>
+      <div><Routes>
+      
+      
+        <Route path='/customers' element= {<CustomerRegistration />} />
+        <Route path='/employees' element= {<EmployeeRegistration />} />
+        <Route path='/sale' element= {<SaleRegistration />} />
+        <Route path='/stock' element= {<StockRegistration />} />
+        <Route path='/user' element= {<UserRegistration />} />
+        
+      </Routes>
+      </div>
+    </BrowserRouter>
+    <Footer />
     </main>
+    // <main >
+    //   <Navbar />
+    //   <Sidebar />
+    //   <Hero />
+    //   <CustomerRegistration />
+    //   <EmployeeRegistration />
+    //   <SaleRegistration />
+    //   <StockRegistration />
+    //   <UserRegistration />
+    //   <Footer />
+    // </main>
   )
 }
+
 
 export default App
