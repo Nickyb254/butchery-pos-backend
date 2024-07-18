de367acb3554be6ff7403c397ca194ba17698746
NIXON <nikimghanga@gmail.com>
2024-07-18 09:37:01 +0300

    removed assets from gitignore

---
+++ Z frontEnd/boma/src/components/assets/all_products.js
@@ -0,0 +1,137 @@
+import photo_1 from './photo_1.jpg';
+import photo_2 from './photo_2.jpg';
+import photo_3 from './photo_3.jpg';
+import photo_4 from './photo_4.jpg';
+import photo_5 from './photo_5.jpg';
+import photo_6 from './photo_6.jpg';
+import photo_7 from './photo_7.jpg';
+import photo_8 from './photo_8.jpg';
+import photo_9 from './photo_9.jpg';
+import photo_10 from './photo_10.jpg';
+import photo_11 from './photo_11.jpg';
+import photo_12 from './photo_12.jpg';
+
+
+let all_products = [
+{
+  id: 1,
+  product_name: "Maini",
+  price: 300,
+  mass_bought: 5,
+  mass_available: 10,
+  supplier_name: "Osca",
+  },
+  
+  {
+    id: 2,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+    {
+      id: 3,
+      product_name: "Maini",
+      price: 300,
+      mass_bought: 5,
+      mass_available: 10,
+      supplier_name: "Osca",
+      },
+  
+      
+  {
+    id: 4,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+    
+  {
+    id: 5,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+  
+    {
+      id: 6,
+      product_name: "Maini",
+      price: 300,
+      mass_bought: 5,
+      mass_available: 10,
+      supplier_name: "Osca",
+      },
+  
+  
+      {
+        id: 7,
+        product_name: "Maini",
+        price: 300,
+        mass_bought: 5,
+        mass_available: 10,
+        supplier_name: "Osca",
+        },
+  
+        
+  {
+    id: 8,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+
+    
+  {
+    id: 9,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+
+    
+  {
+    id: 10,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+
+    
+  {
+    id: 11,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+
+    
+  {
+    id: 12,
+    product_name: "Maini",
+    price: 300,
+    mass_bought: 5,
+    mass_available: 10,
+    supplier_name: "Osca",
+    },
+  
+  
+]
