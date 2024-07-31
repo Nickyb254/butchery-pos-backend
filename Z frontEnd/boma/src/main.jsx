d92e88296aa839ee289faa93fbc81e30162d7b57
Nickyb254 <nikimghanga@gmail.com>
2024-07-31 21:59:01 +0300

    added authContext & AuthProvider

--- Z frontEnd/boma/src/main.jsx
+++ Z frontEnd/boma/src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthProvider.jsx'
 // import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
     <BrowserRouter>
-      <Routes>
-       <Route path="/*" element={<App /> }/> 
-      </Routes>   
+    <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App /> }/> 
+        </Routes>   
+      </AuthProvider>
     </BrowserRouter> 
   </React.StrictMode>,
 )
