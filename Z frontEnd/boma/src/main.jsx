8680ca6a163b98aa619afe5deb24fd2718a8ae07
Nickyb254 <nikimghanga@gmail.com>
2024-08-04 23:58:27 +0300

    improved

--- Z frontEnd/boma/src/main.jsx
+++ Z frontEnd/boma/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
     <BrowserRouter>
-    <AuthProvider>
+      <AuthProvider>
         <Routes>
           <Route path="/*" element={<App /> }/> 
         </Routes>   
