7db9006e09681f58dedb17c93a0af7ff37f5ff4d
Nickyb254 <nikimghanga@gmail.com>
2024-08-13 22:53:31 +0300

    updated employee routes

--- backend/api/routes/employeesRoutes.js
+++ backend/api/routes/employeesRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import checkAuth from '../middleware/check-auth.js';
-import {getAllEmployees} from '../controllers/employees.js';
+import {employeeLogIn, getAllEmployees} from '../controllers/employees.js';
 import {createEmployee} from '../controllers/employees.js';
 import {getOneEmployee} from '../controllers/employees.js';
 import {updateEmployee} from '../controllers/employees.js';
@@ -9,9 +9,10 @@ import {deleteEmployee} from '../controllers/employees.js';
 
 //controller
 router.get('/', getAllEmployees);
-router.post('/',  createEmployee);  //checkAuth,
+router.post('/signup',  createEmployee);  //checkAuth,
+router.post('/login', employeeLogIn)
 router.get('/:employeeId', getOneEmployee);
-router.patch('/:employeesId', checkAuth, updateEmployee);
-router.delete('/:employeesId', checkAuth, deleteEmployee);
+router.patch('/:employeesId',  updateEmployee);//checkAuth,
+router.delete('/:employeesId',  deleteEmployee);//checkAuth,
 
 export default router;
