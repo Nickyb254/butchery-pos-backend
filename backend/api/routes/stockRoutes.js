ec29349f17ee91f2631c75bf9ab83616b54d12cc
Nickyb254 <nikimghanga@gmail.com>
2024-08-21 22:10:04 +0300

    moved upload middleware to server; upload feature working

--- backend/api/routes/stockRoutes.js
+++ backend/api/routes/stockRoutes.js
@@ -6,45 +6,12 @@ import {createStockItem} from '../controllers/stock.js';
 import {updateStock} from '../controllers/stock.js';
 import {deleteStockItem} from '../controllers/stock.js';
 
-import multer from 'multer';
-//adjust how files are stored
-const storage = multer.diskStorage({
-  destination: function(req, file, callback){
-    callback(null, './uploads/');
-  },
-  filename: function(req, file, callback){
-    callback(null, file.originalname)
-    // callback(null, new Date().toISOString() + file.originalname);
-  }
-});
-
-const fileFilter = (req, file, callback) =>{
-//accept file
-  if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/webp"){
-    callback(null, true);
-  } else {
-    callback(null, false);
-    return callback(new Error('Only .png, .jpg and .jpeg format allowed!'));
-    //reject file
-  }  
-};
-
-//simple way
-//const upload = multer({dest: 'uploads/'});
-const upload = multer({
-  storage: storage, 
-  limits: {
-  fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
-
 
 
 //controllers
 router.get('/', getAllStock);
-router.post('/', checkAuth, upload.single('stockImage'), createStockItem);
-router.patch('/:stockId', checkAuth, updateStock );
-router.delete('/:stockId', checkAuth, deleteStockItem);
+router.post('/', createStockItem); //checkAuth,
+router.patch('/:stockId',  updateStock );//checkAuth,
+router.delete('/:stockId',  deleteStockItem);//checkAuth,
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import salesRoutes from './api/routes/salesRoutes.js';
 import stockRoutes from './api/routes/stockRoutes.js';
 import morgan from "morgan";
 import userRoutes from './api/routes/userRoutes.js';
+// import imagesRoutes from './api/routes/imagesRoutes.js'
 import { configDotenv } from "dotenv";
 import cors from 'cors';
 
@@ -19,10 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Frontend URL (adjust for production)
+  origin: 'http://localhost:5173', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: false // Enable cookies and authorization headers with requests
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: false
 };
 
 app.use(cors(corsOptions));
@@ -31,9 +32,70 @@ app.use(morgan('dev'));
 //app.use(bodyParser.urlencoded({extended: true}));
 //app.use(bodyParser.json());
 
-//making uploads folder publicly accessing for GET
-app.use('/uploads' ,express.static('uploads'));
 
+// ------------------------------------------------------------------------------------
+import multer from 'multer';
+//adjust how files are stored
+const storage = multer.diskStorage({
+  destination: function(req, file, callback){
+    callback(null, '../Z frontEnd/boma/src/images/');
+  },
+  filename: function(req, file, callback){
+    callback(null, Date.now() + '--' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, callback) =>{
+//accept file
+  if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/webp"){
+    callback(null, true);
+  } else {
+    callback(null, false);
+    return callback(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    //reject file
+  }  
+};
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+  fileSize: 1000000
+  },
+  fileFilter: fileFilter
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+import ImageModel from "./api/models/images.js";
+import mongoose from "mongoose";
+
+const Images = mongoose.model('Images')
+app.post('/upload-image',  upload.single('image'), async(request, response, next) => {
+  console.log(request.body)
+  const imageName = request.file.filename
+  try {
+    await Images.create({imagez: imageName})
+    // response.status(200).json({message: '1 image file uploaded!'})
+  } catch (error) {
+    response.json({status: error})
+    
+  }
+  next()
+})
+//making uploads folder publicly accessing for GET
+app.use('/' ,express.static('uploads'));
+
+function errHandler(err, req, res, next){
+  if (err instanceof multer.MulterError){
+    res.json({
+      success: 0,
+      message: err.message
+    })
+  }
+}
+
+app.use(errHandler)
+// ------------------------------------------------------------------------------------
 
 //CORS errors- CROSS-ORIGIN RESOURCE SHARING
 app.use((request, response, next) => {
@@ -51,13 +113,14 @@ connectDB();
 app.use('/customers', customerRoutes);
 app.use('/employees', employeesRoutes);
 app.use('/sales', salesRoutes);
-app.use('/stock', stockRoutes);
+app.use('/stock', upload.single('image'), stockRoutes);
 app.use('/customers/Id', customerRoutes);
 app.use('/employees/Id', employeesRoutes);
 app.use('/sales/Id', salesRoutes);
 app.use('/stock/Id', stockRoutes);
 app.use('/user', userRoutes);
 app.use('/login', userRoutes);
+// app.use('/images', imagesRoutes);
 
 //handling any request not in the above routers
 app.use((request, response, next)=>{
