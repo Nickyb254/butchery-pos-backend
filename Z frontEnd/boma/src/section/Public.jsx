d293901bc69a9e75dd6ce78b816ba0ba557b0511
Nickyb254 <nikimghanga@gmail.com>
2024-08-22 23:08:31 +0300

    added products on home page

--- Z frontEnd/boma/src/section/Public.jsx
+++ Z frontEnd/boma/src/section/Public.jsx
@@ -1,11 +1,15 @@
+import  React, { useEffect, useState }  from 'react';
+import axiosInstance from '../api/axios';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import Footer from './Footer';
-import ProductDisplay from '../components/Products/ProductDisplay';
+// import ProductDisplay from '../components/Products/ProductDisplay';
 import photo_1 from '../components/assets/photo_1.jpg';
 import photo_2 from '../components/assets/photo_2.jpg';
 import photo_3 from '../components/assets/photo_3.jpg';
 import Container from 'react-bootstrap/esm/Container';
+import ProductCard from '../components/Products/ProductCard';
+import { Row } from 'react-bootstrap';
 
 function Public () {
   
@@ -13,6 +17,34 @@ function Public () {
     height: '80vh', objectFit: 'cover'
   }
   
+  // const carts = useSelector(store => store.cart.items);
+  //   console.log(carts)
+    const [stock, setStock] = useState([])
+    const [fetchData, setFetchData] = useState(true); // State to trigger data re-fetch     
+    
+    // const dispatch = useDispatch()
+
+    // const handleAddToCart = () => {
+    //     dispatch(addToCart({
+    //         productId: 1,
+    //         quantity: 1
+    //     }))};
+        
+    //fetch All Stock
+    useEffect(() => {
+        if(fetchData){
+             axiosInstance.get('/stock')
+        .then(response => {
+            setStock(response.data.doc)
+            setFetchData(false); // Reset fetchData to avoid continuous re-fetch       
+        })      
+        .catch((error) => {
+            console.log(error);
+        })
+        }
+    },[fetchData])
+
+    
   return (
     <>
       <Card className="text-center bg-dark text-white">
@@ -45,7 +77,14 @@ function Public () {
         </Carousel>
       </Container>
       <div><h1>Our Products</h1> </div>
-        <ProductDisplay/>
+        
+      <Container>
+        <Row>
+            {stock.map((product, key) => 
+                <ProductCard key={key} product={product}/>
+            )}
+            </Row>
+        </Container>
       <Footer/>
       </Card>
     </>
