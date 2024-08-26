9d9ba785b2628ce727e5f4236974ee79595fe14c
Nickyb254 <nikimghanga@gmail.com>
2024-08-26 22:05:44 +0300

    layout from router-dom for

--- Z frontEnd/boma/src/Layout.jsx
+++ Z frontEnd/boma/src/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import MyNavbar from "./components/Navbar";
 
 import React from 'react'
 
 const Layout = () => {
   return (
+    <>
+    <MyNavbar/>
     <Outlet/>
+    </>
   )
 }
 
