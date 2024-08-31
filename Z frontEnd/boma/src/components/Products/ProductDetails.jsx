c452ef6589f819e590ab44f18004657d311d16cf
Nickyb254 <nikimghanga@gmail.com>
2024-08-31 22:20:04 +0300

    displaying product and showing details about it

--- Z frontEnd/boma/src/components/Products/ProductDetails.jsx
+++ Z frontEnd/boma/src/components/Products/ProductDetails.jsx
@@ -2,69 +2,41 @@ import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom' 
 import { Container, Card, Row, Col, Badge, Button } from 'react-bootstrap';
-import { addToCart } from '../../store/store';
-import { useContext } from 'react';
-import DataContext from '../../context/DataProvider';
+
 
 const ProductDetails = () => {
-    const { productId } = useParams();
-    const [product, setProduct] = useState([])
-    const [quantity, setQuantity] = useState(1);
-    
-    const {stock} = useContext(DataContext)
-    // console.log(productId)
-    const dispatch = useDispatch()
+const stock = useSelector((state)=>state.stock.stock) 
 
-    useEffect(()=>{
-        const cartProduct = stock.filter(product => product._id === productId )
-        if(cartProduct.length > 0){
-            setProduct(cartProduct[0])
-        }else{
-            console.log('No Product to send to cart')
-        }
-    },[productId])
+const findItembyId = (id)  => {
+  return stock?.find(stock => stock._id === productId)
+}
+const {productId} = useParams()
+const item = findItembyId(productId)
 
-    //changing cart quantity
-    const handleMinusQuantity = () => {
-        setQuantity(quantity - 1 < 1 ? 1 : quantity - 1);
-    }
-    const handlePlusQuantity = () => {
-        setQuantity(quantity + 1);
-    }
-    const handleAddToCart = () => {
-        dispatch(addToCart({
-            _id: product._id,
-            quantity: quantity
-        }));
-    }
 
-    //testing the addition of arbitrary quantities to cart
-    const carts = useSelector(store => store.cart.items);
-    // console.log(carts)
 
+
+// console.log(product)
   return (
     <div>
-        <Container>
-            <Row>
-            <h1>PRODUCT DETAILS</h1>
-              
-            <Card style={{ width: '40%', height: '100%' }} >              
-                <Card.Img variant="top" src={`http://localhost:5173/src/images/${product.stock_image}`}  style={{ height: '23em', objectFit: 'cover' }} />
-              
-                    <Card.Body>
-                        <Card.Title>{product.product_name}</Card.Title>
-                        <h3>Price: {product.price}</h3>
-                        <Card.Text>         
-                        <small><Button onClick={handleMinusQuantity}> - </Button> </small> 
-                        <Button>{quantity}</Button>
-                        <small><Button onClick={handlePlusQuantity}> + </Button> </small> 
-                        <Button onClick={handleAddToCart}> Add to Cart </Button>
-                        </Card.Text>
-                    </Card.Body>
-            </Card>
-            </Row>
-        </Container>
-    </div>
+    <Container>
+        <Row>
+        <h1>CHECK OUT</h1>
+          
+        <Card style={{ width: '40%', height: '100%' }} >              
+            <Card.Img variant="top" src={`http://localhost:5173/src/images/${item?.stock_image}`}  style={{ height: '23em', objectFit: 'cover' }} />
+          
+                <Card.Body>
+                    <Card.Title>{item.product_name}</Card.Title>
+                    <h3>Price: {item.price}</h3>
+                    <Card.Text>         
+                    <p>All product details here</p>
+                    </Card.Text>
+                </Card.Body>
+        </Card>
+        </Row>
+    </Container>
+</div>
   )
 }
 
--- Z frontEnd/boma/src/components/Products/ProductDisplay.jsx
+++ Z frontEnd/boma/src/components/Products/ProductDisplay.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { Container, Row, Col, Badge, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from '../../components/Cart/cartSlice';
+import { useNavigate } from 'react-router-dom';
 
 function ProductDisplay() {
     const stock = useSelector(state => state.stock.stock);
@@ -10,10 +11,16 @@ function ProductDisplay() {
     const dispatch = useDispatch()
 
     
+   const navigate = useNavigate()
+   
+   const goToProductDetails = (productId)=>{
+    navigate(`/${productId}`)
+   }
     
     
 
   const renderCard = (product, index) => {
+    let productId = product?._id
     
     const handleAddToCart = (product) => {
         dispatch(addToCart(product));
@@ -22,13 +29,13 @@ function ProductDisplay() {
         return(
         <Col md={4} lg={3} className="mb-4" key={product._id}>
         <Card style={{ width: '18rem', margin: '1.25rem' }} >
-            <Card.Img variant="top" src={`http://localhost:5173/src/images/${product.stock_image}`} style={{ width: '17em', margin: '0.25em', height:'13em' }} />
+            <Card.Img variant="top" src={`http://localhost:5173/src/images/${product.stock_image}`} style={{ width: '17em', margin: '0.25em', height:'13em' }} onClick={()=>goToProductDetails(productId)} />
                 <Card.Body>
                     <Card.Title>{product.product_name}</Card.Title>
                     <h3>Price: {product.price}</h3>
                     <Card.Text>         
                     <small><Button onClick={() => handleAddToCart(product)}> Add to Cart</Button> </small> <br/>
-                    <Badge > View </Badge>
+                    <Badge onClick={()=>goToProductDetails(productId)}> View </Badge>
                     </Card.Text>
                 </Card.Body>
         </Card>
