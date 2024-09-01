17d83067bc346cb76d6cafa02cd97f0e530a6023
Nickyb254 <nikimghanga@gmail.com>
2024-09-01 23:36:57 +0300

    displaying cart item details well

--- Z frontEnd/boma/src/components/Cart/CartItem.jsx
+++ Z frontEnd/boma/src/components/Cart/CartItem.jsx
@@ -1,32 +1,26 @@
-import React, {useState, useEffect} from 'react' 
-import {  useDispatch, useSelector } from 'react-redux' 
-import { changeQuantity, addTotalCost, cartTotalValue } from './cartSlice';
+import React from 'react' 
+import {  useSelector } from 'react-redux' 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 // import { useContext } from 'react';
 // import DataContext from '../../context/DataProvider';
-import { useNavigate } from 'react-router-dom';
 
 const CartItem = ({item}) => {
-   const dispatch = useDispatch()
 
-   const navigate = useNavigate()
-   let productId = item?._id
-   const goToProductDetails = (productId)=>{
-    navigate(`/${productId}`)
-   }
 
    const cartItem = useSelector((state)=> state.cart.cartItems)
     let quantity =cartItem[0].quantity
     const price = item?.price
     
     const cost = price * quantity
-    dispatch(cartTotalValue({price, quantity}))
+    
+    
+    
     return (
         <Container className="bg-dark text-white p-2 border-bottom border-secondary rounded">
             <Row className="align-items-center">
                 <Col xs="auto">
-                    <img src={`http://localhost:5173/src/images/${item?.stock_image}`} alt="" className="w-25" onClick={()=>goToProductDetails(productId)} />                    
+                    <img src={`http://localhost:5173/src/images/${item?.stock_image}`} alt="" className="w-25" />
                 </Col>
                 <Col><h4>{item?.product_name}</h4></Col>
                 <Col><p>Price ${item?.price}</p></Col>
