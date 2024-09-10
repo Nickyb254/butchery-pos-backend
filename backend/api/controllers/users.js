69810a656186370283016245c1211489d18784d2
Nickyb254 <nikimghanga@gmail.com>
2024-09-10 22:13:58 +0300

    log out and token refresh APIs working

--- backend/api/controllers/users.js
+++ backend/api/controllers/users.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import userModel from '../models/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import asyncErrorHandler from '../../utils/asyncErrorHandler.js';
 
 
 export const createUser = (request, response, next) => {
@@ -28,7 +29,7 @@ export const createUser = (request, response, next) => {
           user
           .save()
           .then(result => {
-            console.log(result);
+            console.log('create user result',result);
             response.status(201).json({
               message: 'User created!'
             });
@@ -46,10 +47,10 @@ export const createUser = (request, response, next) => {
   }
 
 
+// LOG IN...................................................................................
 
-
-  export const userLogIn = async (request, response, next) => {
-    try {
+  export const userLogIn = asyncErrorHandler(async (request, response, next) => {
+  
       const users = await userModel.find({ email: request.body.email }).exec();
   
       if (users.length < 1) {
@@ -69,77 +70,98 @@ export const createUser = (request, response, next) => {
   
       const accessToken = jwt.sign(
         {email: user.email, userId: user.user_id },
-      `${process.env.JWT_SECRET}`,
-        {expiresIn: "1h"}
+        process.env.JWT_SECRET_KEY,
+        {expiresIn: "13s"}
       );
 
 
       const refreshToken = jwt.sign(
-        { userId: user.user_id  },
-        `${process.env.REFRESH_TOKEN_SECRET}`,
-        { expiresIn: '7d' }
+        {email: user.email, userId: user.user_id  },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '15min' }
     )
 
     // Create secure cookie with refresh token 
-    response.cookie('jwt', refreshToken, {
-        httpOnly: true, //accessible only by web server 
-        secure: true, //https
-        sameSite: 'None', //cross-site cookie 
-        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
-    })
+    response.cookie('jwt', refreshToken, {httpOnly: true, secure: false, sameSite: 'None', maxAge: 15 * 60 * 1000 })
 
-
-
+    // Saving refreshToken with current user //cookie expiry: set to match rT 7 * 24 * 60 * 60 * 1000
+    user.refreshToken = refreshToken
+    const result = user.save()
+    console.log('New refresh token saved with user', result)
   
       return response.status(200).json({
          accessToken
       });
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({
-        error: error
-      });
-    }
-}
+    } 
+)
     
 //security weakness if
 // return res.status(404).json({
 //message: 'Mail not found, user doesn\'t exist'})
+// .......................................................................................
+export const handleRefreshToken = asyncErrorHandler(async (request, response) => {
+  const cookies = request.cookies
 
-export const refresh = (req, res) => {
-  const cookies = req.cookies
-
-  if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
+  if (!cookies?.jwt) return response.sendStatus(401).json({ message: 'Unauthorized! The user has no cookie' })
 
   const refreshToken = cookies.jwt
+  const currentUser = await userModel.findOne({ refreshToken }).exec()
+  // console.log('This the currentUser:',currentUser)
+  if (!currentUser) return response.status(403).json({ message: 'Forbidden! Not a valid user' })
 
   jwt.verify(
       refreshToken,
       process.env.REFRESH_TOKEN_SECRET,
-      async (err, decoded) => {
-          if (err) return res.status(403).json({ message: 'Forbidden' })
-
-          const currentUser = await User.findOne({ userId: decoded.user.user_id }).exec()
-
-          if (!currentUser) return res.status(401).json({ message: 'Unauthorized' })
-
+      (err, decoded) => {
+          // if (err || currentUser._id !== decoded._id ) return response.status(403).json({ message: 'Forbidden! User not found' })
+            console.log('this is the decoded from JWT verify', decoded)
+            if (err) {
+              // Handle errors, e.g., invalid token, expired token
+              console.log('Error from decoding JWT',err)
+              return response.status(403).json({ message: 'Forbidden! Invalid or expired token' });
+          }
+      
+          if (currentUser._email !== decoded._email) {
+              // Handle unauthorized access
+              return response.status(403).json({ message: 'Forbidden! User not found' });
+          } else {
+           // Generate new access token
           const accessToken = jwt.sign(
-              {
-                  "UserInfo": {
-                      "userId": currentUser.user_id,
-                      "roles": currentUser.roles
-                  }
-              },
-              process.env.ACCESS_TOKEN_SECRET,
-              { expiresIn: '15m' }
-          )
-
-          res.json({ accessToken })
+              {"userId": currentUser.user_id },
+              process.env.JWT_SECRET_KEY,
+              { expiresIn: '13s' }
+            )
+            response.status(200).json({message:'This is the new access Token for the user', accessToken })
+        }
       }
   )
-}
+})
 
+//log out........................................................................
+export const logOut = asyncErrorHandler(async (request, response) => {
+  
+    const cookies = request.cookies
+    if(!cookies?.jwt) return response.sendStatus(204) //no content
+    const refreshToken = cookies.jwt
 
+  //find refreshToken in Db
+  const onlineUser = await userModel.findOne({refreshToken}).exec()
+  if(!onlineUser){
+    response.clearCookie('jwt', {httpOnly: true, sameSite: 'None', secure: false})
+    return response.sendStatus(204)
+  }
+
+  //delete refreshToken from db
+  onlineUser.refreshToken = ''
+  const result = await onlineUser.save()
+  console.log('logged out user has no refreshToken', result)
+
+  response.clearCookie('jwt',{httpOnly: true, sameSite: 'None', secure:false})
+  response.sendStatus(204)
+  
+})
+
+//delete user........................................................................
 export const deleteUser = (request, response, next) => {
   userModel.deleteOne({ _id: request.params.userId })
   .exec()
@@ -149,7 +171,7 @@ export const deleteUser = (request, response, next) => {
     });
   })
   .catch(error => {
-    console.log(error);
+    console.log('delete user', error);
     response.status(500).json({
       error: error
     });
