c25decf5a54bf064cae0e26c3bd3d8de6b03492f
Nickyb254 <nikimghanga@gmail.com>
2024-09-12 22:21:15 +0300

    stock slice availing data to components

---
+++ Z frontEnd/boma/src/Features/Stock/stockSlice.jsx
@@ -0,0 +1,57 @@
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiSlice } from "../../api/apiSlice";
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+
+const stockAdapter = createEntityAdapter({})
+
+const initialState = stockAdapter.getInitialState()
+
+
+const stockApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getStock: builder.query({
+            query: () => '/stock',
+            validateStatus: (response, result) => {                
+                return response.status === 200 && !result.isError
+            },
+            // keepUnUsedDataFor: 5,
+            transformResponse: responseData => {               
+                const loadedStock = responseData.doc.map(stock =>{
+                    stock.id = stock._id                    
+                    return stock
+                })               
+                return stockAdapter.setAll(initialState, loadedStock)
+            },
+            providesTags: (result, error, arg) => {                                              
+                if(result?.ids){
+                   return [
+                        {type: 'stock', id: 'LIST'},
+                        ...result.ids.map(id => ({type:'stock', id}))
+                    ]
+                } else {return [{type:'stock', id:'LIST'}] }
+            }
+        }),
+    })
+})
+
+export const {useGetStockQuery} = stockApiSlice;
+
+
+//this is the query result object
+export const selectStockResult = stockApiSlice.endpoints.getStock?.select
+
+//creates a memoized selector
+const selectStockData = createSelector(
+    selectStockResult,
+    stockResult => stockResult.data    //normalized state object with ids and entries
+)
+
+
+// get selectors creates all selectors automatically, we only have to rename them in destructuring
+export const {
+    selectAll: selectAllStock,
+    selectById: selectStockById,
+    selectIds: selectStockIds
+} = stockAdapter.getSelectors(state => selectStockData(state) ?? initialState )
+
+export default stockApiSlice;
