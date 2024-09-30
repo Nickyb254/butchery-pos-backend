c5deeca6d67e6bb94794044cdc33e5f629eff478
Nickyb254 <nikimghanga@gmail.com>
2024-09-30 14:46:57 +0300

    axios configuration, authorization still missing

--- Z frontEnd/boma/src/api/axios.jsx
+++ Z frontEnd/boma/src/api/axios.jsx
@@ -1,44 +1,25 @@
 import axios from 'axios';
+// import { useAuth } from '../context/AuthProvider'; // Adjust the import path
 const BASE_URL ="http://localhost:3000"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL, 
+  // withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
+  // timeout: 10000,
 });
 
-export default axiosInstance;
+export default axiosInstance
 
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL, 
   headers: { 'Content-Type': 'application/json' },
-  withCredentials: true
-});
+  withCredentials: true,
+})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// Add a request interceptor to include the token in the headers
-// axiosInstance.interceptors.request.use(
-//   config => {
-//     const token = localStorage.getItem('token'); // Or sessionStorage.getItem('token');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   }
-// );
 
 
