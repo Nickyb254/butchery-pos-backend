cd299738a1ae9c8eab03012021e084f0c7046987
Nickyb254 <nikimghanga@gmail.com>
2024-09-30 14:39:31 +0300

    redux store providing all state. keeping data provider only for refrences

--- Z frontEnd/boma/src/main.jsx
+++ Z frontEnd/boma/src/main.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthProvider.jsx'
-// import './index.css'
+// import { AuthProvider } from './context/AuthProvider.jsx'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { store } from './app/index.js'
+// import { fetchStock } from './Features/Stock/stockSlice.jsx'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify';
+// import { DataProvider } from './context/DataProvider.jsx';
+
+// store.dispatch(fetchStock())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
     <BrowserRouter>
-      <AuthProvider>
+    <Provider store={store}>
+      {/* <DataProvider> */}
+           <ToastContainer />  
+      {/* <AuthProvider> */}
         <Routes>
           <Route path="/*" element={<App /> }/> 
-        </Routes>   
-      </AuthProvider>
+        </Routes> 
+        {/* </AuthProvider> */}
+      {/* </DataProvider> */}
+      </Provider>
     </BrowserRouter> 
   </React.StrictMode>,
 )
