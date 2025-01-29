7878a8cbbbb85e13327041875e0a2ba6952d4f56
Nickyb254 <nikimghanga@gmail.com>
2025-01-29 13:47:55 +0300

    changed package json for render deployment

--- backend/config/boma_db.js
+++ backend/config/boma_db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
  
 export default async function connectDB() {
-  const url = process.env.MONGO_URL;
+  // const url = process.env.MONGO_URL;
+  const url = process.env.MONGO_ATLAS_URL;
   if (!url) {
     console.error("MONGO_URI is not defined in .env file");
     process.exit(1);
