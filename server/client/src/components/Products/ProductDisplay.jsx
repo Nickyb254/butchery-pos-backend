673fe291dac3bb7b64a048dc7be4f69c0643823b
Nickyb254 <nikimghanga@gmail.com>
2025-02-03 11:43:24 +0300

    image URL resolved based on the deployed domain by accessing window object

--- server/client/src/components/Products/ProductDisplay.jsx
+++ server/client/src/components/Products/ProductDisplay.jsx
@@ -33,7 +33,7 @@ function ProductDisplay({stock}) {
         return(
         <Col md={4} lg={3} className="mb-4" key={product._id}>
         <Card style={{ width: '18rem', margin: '1.25rem' }} >
-            <Card.Img variant="top" src={`/images/${product.stock_image}`} style={{ width: '17em', margin: '0.25em', height:'13em' }} onClick={()=>goToProductDetails(productId)} />
+            <Card.Img variant="top" src={`${window.location.origin}/images/${product.stock_image}`} style={{ width: '17em', margin: '0.25em', height:'13em' }} onClick={()=>goToProductDetails(productId)} />
                 <Card.Body>
                     <Card.Title>{product.product_name}</Card.Title>
                     <h3>Price: {product.price}</h3>
