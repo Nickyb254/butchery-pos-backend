3d10fcac169ed7a5e6f3f6e21c3416b234e48f76
Nickyb254 <nikimghanga@gmail.com>
2025-02-03 14:34:44 +0300

    removed vite proxy configuration for images, served images to public

--- server/server.js
+++ server/server.js
@@ -151,7 +151,7 @@ app.use('/api/v1/orders', orderRoutes);
 
 //Serve frontend as static files
 app.use(express.static(path.join(__dirname, '/client/dist')))
-app.use('/images', express.static(path.join(__dirname, 'client', 'dist', 'images')));
+app.use(express.static(path.join(__dirname, 'client', 'dist', 'images')));
 
 
 //Render client for any path 
