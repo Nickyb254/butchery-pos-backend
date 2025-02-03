89b9cc6a7ff50e67cc7d7ee6be698cc21d27eb6b
Nickyb254 <nikimghanga@gmail.com>
2025-02-03 11:32:59 +0300

    serving images from dist statically after build

--- server/server.js
+++ server/server.js
@@ -151,6 +151,8 @@ app.use('/api/v1/orders', orderRoutes);
 
 //Serve frontend as static files
 app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use('/images', express.static(path.join(__dirname, 'client', 'dist', 'images')));
+
 
 //Render client for any path 
 app.get('*', (req, res)=> res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html')))
